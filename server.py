27baf532d9e1e785a3dc297886fed70d73af6704
nvh95 <nguyenviethunghl00@gmail.com>
2017-10-07 13:43:05 +0700

    Added skeleton for Blockchain server

---
+++ server.py
@@ -0,0 +1,33 @@
+from uuid import uuid4
+
+from blockchain import Blockchain
+
+from flask import Flask, jsonify
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-','')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We will mind a new Block"
+
+@app.route('/transaction/new', methods=['POST'])
+def new_transaction():
+    return "Well will add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
