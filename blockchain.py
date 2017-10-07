f783f6b4dc948a6bc105ef89e2906bd8d7e15341
nvh95 <nguyenviethunghl00@gmail.com>
2017-10-07 11:20:36 +0700

    Define a new block in new_block()

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,32 @@
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transaction = []
 
-    def new_block(self):
-        """Create a new block and adds it the the chain"""
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block and adds it the the chain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> hash of the previous block
+        :return: <dict> a new block
+        """
+        block = {
+            'index': len(self.chain) +1,
+            'timestamp': time(),
+            'transaction': self.current_transaction,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transaction
+        self.current_transaction = []
+
+        self.chain.append(block)
+
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
